f7f3e5a068bad976ce53232403c44ec161d16e78
binbin.han <binbin.han@avatr.com>
2023-12-07 11:27:50 +0800

    modify self annotation

--- app/src/main/java/cn/huolala/mytestapplication/ContentView.java
+++ app/src/main/java/cn/huolala/mytestapplication/ContentView.java
@@ -12,7 +12,13 @@ import java.lang.annotation.Target;
  * Email binbin.han@huolala.cn,
  * Date on 2/26/22.
  * PS: Not easy to write code, please indicate.
- */
+ *
+ * a) 若 Annotation 的类型为 SOURCE，则意味着：Annotation 仅存在于编译器处理期间，编译器处理完之后，该 Annotation 就没用了。 例如，" @Override" 标志就是一个 Annotation。当它修饰一个方法的时候，
+ *    就意味着该方法覆盖父类的方法；并且在编译期间会进行语法检查！编译器处理完后，"@Override" 就没有任何作用了。
+ * b) 若 Annotation 的类型为 CLASS，则意味着：编译器将 Annotation 存储于类对应的 .class 文件中，它是 Annotation 的默认行为。
+ * c) 若 Annotation 的类型为 RUNTIME，则意味着：编译器将 Annotation 存储于 class 文件中，并且可由JVM读入。
+ *
+ * */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface ContentView {
